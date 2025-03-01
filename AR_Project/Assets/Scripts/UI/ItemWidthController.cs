74adae4d904361311a7d9e77825cbce27c56d681
Verfinity <129280114+Verfinity@users.noreply.github.com>
2025-03-01 14:21:08 +0300

    fixed furniture scrollbar's bugs

--- AR_Project/Assets/Scripts/UI/ItemWidthController.cs
+++ AR_Project/Assets/Scripts/UI/ItemWidthController.cs
@@ -1,28 +1,15 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-[RequireComponent(typeof(RectTransform))]
 public class ItemWidthController : MonoBehaviour
 {
-    private SpacingController _parentSpacingController;
-
     private void Start()
     {
-        _parentSpacingController = GetComponentInParent<SpacingController>();
-    }
+        var parentRectTransform = transform.parent.GetComponent<RectTransform>();
+        var rectTransform = GetComponent<RectTransform>();
 
-    private void ChangeWidthScale(float coefficient)
-    {
-        GetComponent<RectTransform>().localScale = new Vector2(coefficient, 1);
-    }
-
-    private void OnEnable()
-    {
-        _parentSpacingController.SpacingDistanceChanged += ChangeWidthScale;
-    }
-
-    private void OnDisable()
-    {
-        _parentSpacingController.SpacingDistanceChanged -= ChangeWidthScale;
+        rectTransform.SetSizeWithCurrentAnchors(
+            RectTransform.Axis.Horizontal, 
+            parentRectTransform.rect.height
+            );
     }
 }
